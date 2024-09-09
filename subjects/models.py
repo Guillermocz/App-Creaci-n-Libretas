930c564a506f2286512b723d83b551f43a3e4659
Guillermo <calvache98@gmail.com>
2024-09-08 20:35:07 -0500

    Creación modelos subjects

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from students.models import Course
 
-# Create your models here.
+class Subject(models.Model):
+    nombre = models.CharField(max_length=100)
+    cursos = models.ManyToManyField(Course)  # Relación muchos a muchos con cursos
+
+    def __str__(self):
+        return self.nombre
